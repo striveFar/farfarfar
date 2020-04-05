73e74a8b61cd168e0ecb30917d5a5217b1b8dca6
youngfar <1254333071@qq.com>
2020-04-05 19:20:20 +0800

    Test confliction written by hot_fix.

--- test.c
+++ test.c
@@ -3,10 +3,11 @@ int main()
 
 {
 	//fixd by hot_fix
+	printf("测试:不同分支修改同一个文件的不同行是否会引起冲突，此行修改于hot_fix分支\n\r");
 	int a=0;
 	printf("a = %d\n\r",a);
 	return -1;//  Confliction test written by hot_fix.
-}
+
 
 
 
